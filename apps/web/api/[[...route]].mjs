dd339443de10acafe9ae013da9ef4dc5066186af
Junior Sartori <sartorijr92@gmail.com>
2026-03-03 22:42:36 -0300

    Fixing vercel configs

--- apps/web/api/[[...route]].mjs
+++ apps/web/api/[[...route]].mjs
@@ -1,8 +1,8 @@
-import { app } from "../../api/src/app";
+import { app } from "./_app.mjs";
 
 export const config = { runtime: "edge" };
 
-export default async function handler(req: Request) {
+export default async function handler(req) {
 	const url = new URL(req.url);
 	url.pathname = url.pathname.replace(/^\/api/, "") || "/";
 	return app.fetch(new Request(url, req));
